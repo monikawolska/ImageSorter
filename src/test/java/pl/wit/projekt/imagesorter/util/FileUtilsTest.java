22d7fccbd675bee1beda0492792294386ea1b05d
Monika <62252218+monikawolska@users.noreply.github.com>
2023-06-19 22:12:42 +0200

    Update FileUtilsTest.java
    add import

--- src/test/java/pl/wit/projekt/imagesorter/util/FileUtilsTest.java
+++ src/test/java/pl/wit/projekt/imagesorter/util/FileUtilsTest.java
@@ -3,6 +3,14 @@
  */
 package pl.wit.projekt.imagesorter.util;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.wit.projekt.imagesorter.util.FileUtils;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * @author 
  *
